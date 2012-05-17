225103a10300995819adb0879d3345a7b8dff405
saitodisse <saitodisse@gmail.com>
2012-05-17 14:45:14 -0300

    14/40

---
+++ Testes/helpers/Helpers.cs
@@ -0,0 +1,11 @@
+﻿namespace KoansCSharpPtBr.helpers
+{
+    public class Helpers
+    {
+        public static object __ = "COMPLETE_AQUI";
+        public static bool ___ = false;
+    }
+    public static class ___
+    {
+    }
+}
---
+++ Testes/helpers/_ZZ_TestePai.cs
@@ -0,0 +1,16 @@
+namespace KoansCSharpPtBr.helpers
+{
+    public class _ZZ_TestePai
+    {
+        protected bool ___
+        {
+            get { return false; }
+            set { throw new System.NotImplementedException(); }
+        }
+        protected object ____
+        {
+            get { return ""; }
+            set { throw new System.NotImplementedException(); }
+        }
+    }
+}
